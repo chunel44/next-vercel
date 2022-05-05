deddbe566a7a2ef48020f333a27b528c5fc977d1
Estrada44 <jesus@mexiconow.mx>
2022-05-04 18:43:10 -0600

    Pre-deployment-Vercel

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import { FC } from 'react';
+import { Navbar } from '../Navbar';
+
+import styles from './MainLayout.module.css';
+
+interface Props { 
+  children?: React.ReactNode | undefined
+ }
+
+export const MainLayout: FC<Props> = ({ children }) => {
+    return (
+        <div className={styles.container}>
+          
+          <Head>
+            <title>Home - Fernando</title>
+            <meta name="description" content="Home Page" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+        
+          <Navbar />
+    
+          <main className={styles.main}>
+    
+            { children }
+    
+          </main>     
+        </div>
+      )
+};
